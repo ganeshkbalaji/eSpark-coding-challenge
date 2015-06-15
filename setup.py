3948e48cf3335ebbd95e0dbef15c433ba91e3fa3
Ganesh Balaji <ganeshkbalaji@ymail.com>
2015-06-15 13:37:34 -0600

    added setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='espark',
+    version='0.0.1',
+    description='Calculates a study path for students.',
+    url='https://github.com/ganeshkbalaji/eSpark-coding-challenge',
+    keywords="espark students",
+    packages=['espark'],
+    scripts=['bin/esparkify'],
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    long_description=open('README.md', 'r').read(),
+    )
